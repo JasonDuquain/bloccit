2651a3666f30c29d96772d9c9b43cbbbdf93af4d
jasonduquain <jduquain@gmail.com>
2019-01-09 12:10:10 -0500

    REVSISION - assignment_into-to-express: completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,33 +2,22 @@ const request = require('request');
 const server = require('../../src/server');
 const base = "http://localhost:3000/";
 
-
-describe('routes : static', () => {
-    describe('GET /', () => {
-        it('should return status code 200', (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                done();
-            });
+describe('GET /', () => {
+    it('should return status code 200', (done) => {
+        request.get(base, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            done();
         });
     });
 });
 
-describe('routes : marco', () => {
-    describe('GET /marco', () => {
-        it('should return status code 200', (done) => {
-            request.get(base + 'marco/', (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                done();
-            });
-        });
-
-        it('should contain the string "polo"', (done) => {
-            request.get(base + 'marco/', (err, res, body) => {
-                expect(body).toContain("polo");
-                done();
-            });
+describe('GET /marco', () => {
+    it('should return status code 200 and have the word polo in the body', (done) => {
+        request.get(base + 'marco/', (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(body).toContain("polo");
+            done();
         });
     });
-})
+});
 

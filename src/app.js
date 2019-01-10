71a4d2c8aad956b2c48a12128cf5bc85e7e516ad
jasonduquain <jduquain@gmail.com>
2019-01-09 22:02:10 -0500

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 
 const express = require("express");
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 const app = express();
 
-
+appConfig.init();
+routeConfig.init(app);
 
 module.exports = app;
+

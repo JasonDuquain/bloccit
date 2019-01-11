1f9258c42794fdafe07dbd2bc79c1267f8e028b5
jasonduquain <jduquain@gmail.com>
2019-01-11 14:37:34 -0500

    checkpoint_routing-controllers-views: completed

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+const staticController = require("../controllers/staticController");
+
+router.get("/", staticController.index);
 
 module.exports = router;

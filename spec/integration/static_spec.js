6603d04677949504b4e41ba4806358215cac55a4
jasonduquain <jduquain@gmail.com>
2019-01-11 23:30:28 -0500

    assignment_routing-controllers-views: completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,17 +2,23 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
-describe("routes : static", () => {
-
-    describe("GET /", () => {
-
-        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                expect(body).toContain("Welcome to Bloccit");
-                done();
-            });
+describe('GET /', () => {
+    it('should return status code 200', (done) => {
+        request.get(base, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            done();
         });
-
     });
 });
+
+describe('GET /about', () => {
+    it('should return status code 200 and have the words About Us in the body', (done) => {
+        request.get(base + 'about/', (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(body).toContain("About Us");
+            done();
+        });
+    });
+});
+
+
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -2,8 +2,13 @@
 module.exports = {
     index(req, res, next) {
         res.render("static/index", {title: "Welcome to Bloccit"});
+    },
+    about(req, res, next) {
+        res.render('static/about', {title: 'About Us'});
     }
 }
+
+
 
 
 

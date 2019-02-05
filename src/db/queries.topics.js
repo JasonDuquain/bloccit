947f339e52db568949a4241327027ee2b7bb8af0
jasonduquain <jduquain@gmail.com>
2019-02-05 11:58:39 -0500

    post-resource-two_checkpoint: completed

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
 
@@ -24,15 +25,18 @@ module.exports = {
                 callback(err);
             })
     },
-    getTopic(id, callback){
-         return Topic.findById(id)
-             .then((topic) => {
-               callback(null, topic);
-             })
-             .catch((err) => {
-               callback(err);
-             })
-   },
+    getTopic(id, callback) {
+        return Topic.findById(id, {
+            include: [{
+                model: Post,
+                as: 'posts'
+            }]
+        }).then((topic) => {
+            callback(null, topic);
+        }).catch((err) => {
+            callback(err);
+        })
+    },
     deleteTopic(id, callback){
          return Topic.destroy({
                where: {id}
